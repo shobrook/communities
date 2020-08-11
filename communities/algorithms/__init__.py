7f9e0186a13f583c22e37da7199d08540ccdb87a
shobrook <shobrookj@gmail.com>
2020-08-11 19:10:19 -0400

    Implemented the Bren-Kerbosch maximal clique detection algorithm

--- communities/algorithms/__init__.py
+++ communities/algorithms/__init__.py
@@ -2,3 +2,4 @@ from .louvain_method import louvain_method
 from .girvan_newman import girvan_newman
 from .hierarchical_clustering import hierarchical_clustering
 from .spectral_clustering import spectral_clustering
+from .bron_kerbosch import bron_kerbosch
---
+++ communities/algorithms/bron_kerbosch.py
@@ -0,0 +1,30 @@
+######
+# MAIN
+######
+
+
+def bron_kerbosch(adj_matrix, pivot=False):
+    maximal_cliques = []
+
+    def N(v):
+        return {i for i, weight in enumerate(adj_matrix[v]) if weight}
+
+    def _bron_kerbosch(R, P, X):
+        if not P and not X:
+            maximal_cliques.append(R)
+        else:
+            if pivot:
+                u = max(P | X, key=lambda i: len(N(i)))
+                _P = P.copy() - N(u)
+            else:
+                _P = P.copy()
+
+            for v in _P:
+                _bron_kerbosch(R | {v}, P & N(v), X & N(v))
+                P.remove(v)
+                X.add(v)
+
+    R, P, X = set(), set(range(len(adj_matrix))), set()
+    _bron_kerbosch(R, P, X)
+
+    return maximal_cliques
