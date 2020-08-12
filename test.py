4c737c041ef453b253b4781051740aa49bd135b0
shobrook <shobrookj@gmail.com>
2020-08-11 23:50:08 -0400

    Deleted tests

--- test.py
+++
@@ -1,7 +0,0 @@
-from communities.algorithms import hierarchical_clustering
-import pickle
-import numpy as np
-
-adj_matrix = pickle.load(open("test_graph.pkl", "rb"))
-communities = hierarchical_clustering(np.array(adj_matrix), size=None, metric="cosine", linkage="mean")
-print(communities)
